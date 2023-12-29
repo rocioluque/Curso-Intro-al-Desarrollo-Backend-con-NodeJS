346752aaf708af3276eeec9139d54a6997c942c8
rocioluque <rocioluq4@gmail.com>
2023-12-29 18:09:15 -0300

    Clase 4 - API Libros

---
+++ Clase 4 - API Libros/data.js
@@ -0,0 +1,19 @@
+const libros = [
+  {
+    id: '1',
+    titulo: 'El señor de los anillos',
+    autor: 'J.R.R.Tolkien',
+  },
+  {
+    id: '2',
+    titulo: 'Harry Potter y la piedra filosofal',
+    autor: 'J.K. Rowling',
+  },
+  {
+    id: '3',
+    titulo: 'Harry Potter y la camara secreta',
+    autor: 'J.K. Rowling',
+  }
+];
+
+module.exports = libros;
